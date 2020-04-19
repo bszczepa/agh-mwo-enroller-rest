eb345dc529b8f69ff7c92bf0c1c20a8f21305400
Bartek <bar.szczepanski>
2020-04-19 14:45:28 +0200

    part 2 implemented

--- src/main/java/com/company/enroller/persistence/ParticipantService.java
+++ src/main/java/com/company/enroller/persistence/ParticipantService.java
@@ -2,6 +2,9 @@ package com.company.enroller.persistence;
 
 import java.util.Collection;
 
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.springframework.stereotype.Component;
 
 import com.company.enroller.model.Participant;
@@ -9,14 +12,30 @@ import com.company.enroller.model.Participant;
 @Component("participantService")
 public class ParticipantService {
 
-	DatabaseConnector connector;
+	Session session;
 
 	public ParticipantService() {
-		connector = DatabaseConnector.getInstance();
+		session = DatabaseConnector.getInstance().getSession();
 	}
 
 	public Collection<Participant> getAll() {
-		return connector.getSession().createCriteria(Participant.class).list();
+		return session.createCriteria(Participant.class).list();
 	}
 
+	public Participant findByLogin(String participantLogin) {
+		return (Participant) session.get(Participant.class, participantLogin);
+	}
+
+	public Participant add(Participant participant) {
+		Transaction transaction = this.session.beginTransaction();
+		session.save(participant);
+		transaction.commit();
+		return participant;
+	}
+
+	public void delete(Participant participant) {
+		Transaction transaction = this.session.beginTransaction();
+		session.delete(participant);
+		transaction.commit();
+	}
 }
